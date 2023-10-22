15b8d9397b56eca29549d52baa1224620c372e90
Isak <isakulstrup@gmail.com>
2023-10-22 09:55:51 +0200

    minor styling

--- src/Grid.elm
+++ src/Grid.elm
@@ -44,6 +44,37 @@ pointToString ( x, y, z ) =
         ++ String.fromInt z
 
 
+scale : Int -> Point -> Point
+scale factor ( x, y, z ) =
+    ( x * factor, y * factor, z * factor )
+
+
+add : Point -> Point -> Point
+add ( x1, y1, z1 ) ( x2, y2, z2 ) =
+    ( x1 + y1, y1 + y2, z1 + z2 )
+
+
+type alias Directions =
+    { one : Point
+    , two : Point
+    , three : Point
+    , four : Point
+    , five : Point
+    , six : Point
+    }
+
+
+neighbours : Directions
+neighbours =
+    Directions
+        ( 1, 0, -1 )
+        ( 1, -1, 0 )
+        ( 0, -1, 1 )
+        ( -1, 0, 1 )
+        ( -1, 1, 0 )
+        ( 0, 1, -1 )
+
+
 
 -- GRID
 
@@ -75,3 +106,22 @@ fromList items =
 toList : Grid a -> List ( Point, a )
 toList (Grid grid) =
     Dict.toList grid
+
+
+addRing : Point -> Int -> a -> Grid a -> Grid a
+addRing center radius hex (Grid grid) =
+    -- function cube_ring(center, radius):
+    -- var results = []
+    -- # this code doesn't work for radius == 0; can you see why?
+    -- var hex = cube_add(center,
+    --                     cube_scale(cube_direction(4), radius))
+    -- for each 0 ≤ i < 6:
+    --     for each 0 ≤ j < radius:
+    --         results.append(hex)
+    --         hex = cube_neighbor(hex, i)
+    -- return results
+    let
+        h =
+            add center (scale radius neighbours.four)
+    in
+    Grid grid
