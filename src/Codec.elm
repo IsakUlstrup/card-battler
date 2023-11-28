702d27e5bbfa2b96dc58ed9382edb40f4a10f031
Isak <isakulstrup@gmail.com>
2023-11-28 09:58:37 +0100

    added card cost encoder to codec

--- src/Codec.elm
+++ src/Codec.elm
@@ -1,12 +1,27 @@
 module Codec exposing (saveCards)
 
 import Card exposing (Action, Card)
+import CustomDict as Dict exposing (Dict)
+import Energy exposing (Energy)
 import Json.Encode as Encode
 import Ports
 
 
 
 -- ENCODER
+
+
+energyEncoder : ( Energy, Int ) -> Encode.Value
+energyEncoder ( energy, amount ) =
+    Encode.object
+        [ ( "first", Encode.string (Energy.toString energy) )
+        , ( "second", Encode.string (String.fromInt amount) )
+        ]
+
+
+costEncoder : Dict Energy Int -> Encode.Value
+costEncoder cost =
+    Encode.list energyEncoder (Dict.toList cost)
 
 
 actionEncoder : Action -> Encode.Value
@@ -19,6 +34,7 @@ cardEncoder card =
     Encode.object
         [ ( "name", Encode.string card.name )
         , ( "action", actionEncoder card.action )
+        , ( "cost", costEncoder card.cost )
         ]
 
 
