68f15502a94193e71d53e13ac504a4c093231f1c
Isak Ulstrup <isakulstrup@gmail.com>
2023-11-29 13:50:46 +0100

    renamed preset cards removed preset decks

--- src/Content/Cards.elm
+++ src/Content/Cards.elm
@@ -1,9 +1,7 @@
 module Content.Cards exposing
     ( basicCard
     , basicCard2
-    , basicDeck
     , expensiveCard
-    , testDeck1
     )
 
 import Card exposing (Card)
@@ -15,28 +13,14 @@ import Card exposing (Card)
 
 basicCard : Card
 basicCard =
-    Card.new "Basic Attack" (Card.Damage 1) 2
+    Card.new "Tackle" (Card.Damage 1) 2
 
 
 basicCard2 : Card
 basicCard2 =
-    Card.new "Basic Attack2" (Card.Damage 2) 3
+    Card.new "Slam" (Card.Damage 2) 3
 
 
 expensiveCard : Card
 expensiveCard =
-    Card.new "Expensive Attack" (Card.Damage 10) 7
-
-
-
--- DECKS
-
-
-testDeck1 : List Card
-testDeck1 =
-    [ basicCard, basicCard2, expensiveCard, basicCard, basicCard2 ]
-
-
-basicDeck : List Card
-basicDeck =
-    [ basicCard, basicCard2, basicCard, basicCard2, basicCard, basicCard2 ]
+    Card.new "Fireball" (Card.Damage 10) 7
