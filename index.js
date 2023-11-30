0975e575f0ad4306c41e12bf1a6bb504e6b4a46a
Isak <isakulstrup@gmail.com>
2023-11-30 19:14:47 +0100

    disabled debug logs

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import { Elm } from "./src/Main.elm";
 
 // Get state from localstorage
 const storedCards = localStorage.getItem('cards');
-// const cards = storedState ? storedState : null;
-console.log("Retrieved state: ", storedCards);
+// console.log("Retrieved state: ", storedCards);
 
 
 // Elm flags object
@@ -22,7 +21,6 @@ const app = Elm.Main.init({
 
 // storeCards port listener
 app.ports.storeCards.subscribe(function (cards) {
-    // const cardsJson = JSON.stringify(cards);
     localStorage.setItem('cards', cards);
-    console.log("Saved cards: ", cards);
+    // console.log("Saved cards: ", cards);
 });
